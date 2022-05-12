928236b2e32d86435cb6bd9c06ac4839f54ef109
kefkas12 <stevenkaigeru@alumni.mdp.ac.id>
2022-05-12 22:05:55 +0700

    Update Detail_kwitansiController.php

--- app/Http/Controllers/Detail_kwitansiController.php
+++ app/Http/Controllers/Detail_kwitansiController.php
@@ -15,26 +15,6 @@ class Detail_kwitansiController extends Controller
     }
     public function index()
     {
-        $kwitansi = Kwitansi::get();
-        foreach ($kwitansi as $v) {
-            $detail_kwitansi = Detail_kwitansi::where('id_kwitansi', $v->id)->get();
-            $kwitansi = Kwitansi::find($v->id);
-            $total_dpp_kwitansi = 0;
-            $total_pph_kwitansi = 0;
-            $total_ppn_kwitansi = 0;
-            $total_nilai_kwitansi = 0;
-            foreach ($detail_kwitansi as $w) {
-                $total_dpp_kwitansi += $w->dpp;
-                $total_pph_kwitansi += $w->pph;
-                $total_ppn_kwitansi += $w->ppn;
-                $total_nilai_kwitansi += $w->total;
-            }
-            $kwitansi->total_dpp_kwitansi = $total_dpp_kwitansi;
-            $kwitansi->total_pph_kwitansi = $total_pph_kwitansi;
-            $kwitansi->total_ppn_kwitansi = $total_ppn_kwitansi;
-            $kwitansi->total_nilai_kwitansi = $total_nilai_kwitansi;
-            $kwitansi->save();
-        }
 
         if ($_GET['per_page'] == -1) {
             $detail_kwitansi = Detail_kwitansi::count();
