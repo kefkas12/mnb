929d89fe56b0f4af77643c0852322ee79c0aec40
kefkas12 <stevenkaigeru@alumni.mdp.ac.id>
2022-05-19 10:23:23 +0700

    refresh

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Detail_kwitansi;
+use App\Kwitansi;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,5 +26,29 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function refresh(){
+        $kwitansi = Kwitansi::get();
+        foreach ($kwitansi as $v) {
+            $detail_kwitansi = Detail_kwitansi::where('id_kwitansi', $v->id)->get();
+            $kwitansi = Kwitansi::find($v->id);
+            $total_dpp_kwitansi = 0;
+            $total_pph_kwitansi = 0;
+            $total_ppn_kwitansi = 0;
+            $total_nilai_kwitansi = 0;
+            foreach ($detail_kwitansi as $w) {
+                $total_dpp_kwitansi += $w->dpp;
+                $total_pph_kwitansi += $w->pph;
+                $total_ppn_kwitansi += $w->ppn;
+                $total_nilai_kwitansi += $w->total;
+            }
+            $kwitansi->total_dpp_kwitansi = $total_dpp_kwitansi;
+            $kwitansi->total_pph_kwitansi = $total_pph_kwitansi;
+            $kwitansi->total_ppn_kwitansi = $total_ppn_kwitansi;
+            $kwitansi->total_nilai_kwitansi = $total_nilai_kwitansi;
+            $kwitansi->save();
+        }
+        return 'Refresh Done'
     }
 }
