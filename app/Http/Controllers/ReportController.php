5977c494ca4ff48beb0ece476e9bb6a1bdf55a18
kefkas12 <stevenkaigeru@alumni.mdp.ac.id>
2022-07-24 17:48:53 +0700

    tes

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -260,7 +260,7 @@ class ReportController extends Controller
 
             // $data['kredit'] = Detail_jurnal_umum::select('tanggal_jurnal as tanggal', 'keterangan', DB::raw('sub_total as kredit'))->where('kode_akun_kredit', '113.101')->whereBetween('tanggal_jurnal', [$from, $to])->where('nama_perusahaan_customer', $customer)->orderBy('created_at', 'DESC')->get();
 
-            $data['report'] = Detail_jurnal_umum::select('tanggal_jurnal', 'kode_akun_debit', 'kode_akun_kredit', 'keterangan', DB::raw('if(kode_akun_debit = "113.101", sub_total, 0)as debit'), DB::raw('if(kode_akun_kredit = "113.101", sub_total, 0) as kredit'))->whereBetween('detail_jurnal_umum.tanggal_jurnal', [$from, $to])->Where('kode_akun_kredit', '113.101')->orWhere('kode_akun_debit', '113.101')->where('nama_perusahaan_customer', $customer)->orderBy('tanggal_jurnal', 'ASC')->get();
+            $data['kredit'] = Detail_jurnal_umum::select('tanggal_jurnal', 'kode_akun_debit', 'kode_akun_kredit', 'keterangan', DB::raw('if(kode_akun_debit = "113.101", sub_total, 0)as debit'), DB::raw('if(kode_akun_kredit = "113.101", sub_total, 0) as kredit'))->where('nama_perusahaan_customer', $customer)->whereBetween('detail_jurnal_umum.tanggal_jurnal', [$from, $to])->Where('kode_akun_kredit', '113.101')->orWhere('kode_akun_debit', '113.101')->orderBy('tanggal_jurnal', 'ASC')->get();
 
             $kwitansi_awal = Customer::where('nama_perusahaan', $customer)->first();
 
