f5377e63139cd6bec6569f567de6a8e316abaf9b
kefkas12 <51503218+kefkas12@users.noreply.github.com>
2022-08-09 16:43:19 +0700

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -844,9 +844,12 @@ class ReportController extends Controller
 
         $data['pendapatan_bunga_bank'] = $saldo_awal_perkiraan->saldo_awal_debit + $saldo_awal + $jm + $pendapatan_bunga_bank;
 
+        //pembelian
+        $saldo_awal_perkiraan = Perkiraan::select('kode_akun', 'nama_perkiraan', 'normal_balance', DB::raw('cast(saldo_awal_debit as decimal(65,2)) as saldo_awal_debit'), DB::raw('cast(saldo_awal_kredit as decimal(65,2)) as saldo_awal_kredit'))->Where('kode_akun', '610.001')->orderBy('kode_akun', 'ASC')->first();
+
         $data['pembelian'] = Detail_kwitansi::select(DB::raw('SUM(berat_bersih*harga_beli) as pembelian'))->whereBetween('tanggal_tagihan', [$from, $to])->first();
 
-        $data['pembelian'] = $data['pembelian']->pembelian;
+        $data['pembelian'] = $saldo_awal_perkiraan->saldo_awal_debit + $data['pembelian']->pembelian;
 
         // $data['biaya_operasional'] = Detail_jurnal_umum::select(DB::raw('cast(sum(sub_total) as decimal(65,2)) as saldo'), 'kode_akun_debit', 'detail_kode_akun_debit')->whereBetween('detail_jurnal_umum.tanggal_jurnal', [$from, $to])->Where('kode_akun_debit', 'like', '51%')->groupBy('kode_akun_debit', 'detail_kode_akun_debit')->get();
 
