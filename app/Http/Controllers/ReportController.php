32df3aa9546e2f5d1dfaab5a49999199b41aab5b
kefkas12 <stevenkaigeru@alumni.mdp.ac.id>
2022-08-13 15:18:46 +0700

    neraca

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -869,17 +869,26 @@ class ReportController extends Controller
             $saldo_awal_kredit = Detail_jurnal_umum::leftJoin('jurnal_umum', 'detail_jurnal_umum.id_jurnal_umum', '=', 'jurnal_umum.id')->select(DB::raw('sum(detail_jurnal_umum.sub_total) as kredit'))->where('detail_jurnal_umum.kode_akun_kredit', $v->kode_akun)->where('jurnal_umum.tanggal_jurnal', '<', $from)->first();
 
             $debit = Detail_jurnal_umum::select(DB::raw('cast(sum(sub_total) as decimal(65,2)) as debit'))->whereBetween('tanggal_jurnal', [$from, $to])->Where('kode_akun_debit', $v->kode_akun)->groupBy('sub_total')->orderBy('detail_jurnal_umum.created_at', 'DESC')->first();
+            if ($debit->debit) {
+                $debit = $debit->debit;
+            } else {
+                $debit = 0;
+            }
 
             $kredit = Detail_jurnal_umum::select(DB::raw('cast(sum(sub_total) as decimal(65,2)) as kredit'))->whereBetween('tanggal_jurnal', [$from, $to])->Where('kode_akun_kredit', $v->kode_akun)->groupBy('sub_total')->orderBy('detail_jurnal_umum.created_at', 'DESC')->first();
 
-            
+            if ($kredit->kredit) {
+                $kredit = $kredit->kredit;
+            } else {
+                $kredit = 0;
+            }
 
             $data_biaya['kode_akun'] = $v->kode_akun;
             $data_biaya['nama_perkiraan'] = $v->nama_perkiraan;
             if($from < '2022-04-01'){
-                $data_biaya['saldo'] = $v->saldo_awal_debit + $debit->debit - $kredit->kredit;
+                $data_biaya['saldo'] = $v->saldo_awal_debit + $debit - $kredit;
             }else{
-                $data_biaya['saldo'] = $debit->debit - $kredit->kredit;
+                $data_biaya['saldo'] = $debit - $kredit;
             }
             // $data_biaya['saldo'] = $v->saldo_awal_debit + $saldo_awal_debit->debit - $saldo_awal_kredit->kredit;
 
