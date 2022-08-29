8166b5edb221a8f26fc5296e8bb3f3290fe1fd82
kefkas12 <51503218+kefkas12@users.noreply.github.com>
2022-08-30 05:17:18 +0700

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -994,7 +994,7 @@ class ReportController extends Controller
         $ppn_masukan = Detail_jurnal_umum::leftJoin('jurnal_umum', 'detail_jurnal_umum.id_jurnal_umum', '=', 'jurnal_umum.id')->select(DB::raw('cast(sum(detail_jurnal_umum.sub_total) as decimal(65,2)) as debit'))->where('detail_jurnal_umum.kode_akun_debit', '115.001')->whereDate('jurnal_umum.tanggal_jurnal', '<=', $to)->first()->debit;
 
         $data['kas'] = number_format($kas_awal + $kas_debit - $kas_kredit, 2, ".", "");
-        $data['bank'] = number_format(round($bank_awal + $bank_debit - $bank_kredit), 2, ".", "");
+        $data['bank'] = number_format($bank_awal + $bank_debit - $bank_kredit, 2, ".", "");
         $data['piutang_dagang'] = number_format($piutang_dagang_awal + $piutang_dagang_debit - $piutang_dagang_kredit, 2, ".", "");
         $data['uang_muka_pajak'] = number_format($uang_muka_pajak, 2, ".", "");
         $data['hutang_dagang'] = number_format($hutang_dagang, 2, ".", "");
