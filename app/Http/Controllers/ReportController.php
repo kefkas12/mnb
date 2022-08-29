556e95a638d2218c1c1fc4ea7702ad23935e74ce
kefkas12 <51503218+kefkas12@users.noreply.github.com>
2022-08-30 04:40:51 +0700

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1033,7 +1033,7 @@ class ReportController extends Controller
 
         $laba_tahun_berjalan = $penjualan + $pendapatan_bunga_bank + $pendapatan_lainnya - $pembelian - $biaya ;
 
-        $laba_ditahan = $laba_ditahan_awal + $laba_tahun_berjalan;
+        $laba_ditahan = $from < '2022-04-01' ? $laba_ditahan_awal : $laba_ditahan_awal + $laba_tahun_berjalan;
 
         $data['laba_ditahan'] = number_format($laba_ditahan, 2, ".", "");
         return $data;
