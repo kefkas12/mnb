6b3a8faccbb76203773625bb0b0d54d3351e5d27
kefkas12 <51503218+kefkas12@users.noreply.github.com>
2022-09-01 00:08:31 +0700

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -987,16 +987,17 @@ class ReportController extends Controller
         $piutang_dagang_debit = Kwitansi::select(DB::raw('cast(sum( total_dpp_kwitansi + total_ppn_kwitansi) as decimal(65,2)) as debit'))->whereDate('tanggal_kwitansi', '<=', $to)->first()->debit;
         $piutang_dagang_kredit = Detail_jurnal_umum::leftJoin('jurnal_umum', 'detail_jurnal_umum.id_jurnal_umum', '=', 'jurnal_umum.id')->select(DB::raw('cast(sum(detail_jurnal_umum.sub_total) as decimal(65,2)) as kredit'))->where('detail_jurnal_umum.kode_akun_kredit', '113.101')->where('jurnal_umum.tanggal_jurnal', '<=', $to)->first()->kredit;
 
-        $ppn_keluaran = Detail_jurnal_umum::leftJoin('jurnal_umum', 'detail_jurnal_umum.id_jurnal_umum', '=', 'jurnal_umum.id')->select(DB::raw('cast(sum(detail_jurnal_umum.sub_total) as decimal(65,2)) as debit'))->where('detail_jurnal_umum.kode_akun_debit', '230.001')->whereDate('jurnal_umum.tanggal_jurnal', '<=', $to)->first()->debit;
+        $ppn_keluaran = Detail_jurnal_umum::leftJoin('jurnal_umum', 'detail_jurnal_umum.id_jurnal_umum', '=', 'jurnal_umum.id')->select(DB::raw('cast(sum(detail_jurnal_umum.sub_total) as decimal(65,2)) as debit'))->where('detail_jurnal_umum.kode_akun_debit', '230.001')->whereBetween('jurnal_umum.tanggal_jurnal', [$from, $to])->first()->debit;
 
-        $ppn_masukan = Detail_jurnal_umum::leftJoin('jurnal_umum', 'detail_jurnal_umum.id_jurnal_umum', '=', 'jurnal_umum.id')->select(DB::raw('cast(sum(detail_jurnal_umum.sub_total) as decimal(65,2)) as debit'))->where('detail_jurnal_umum.kode_akun_debit', '115.001')->whereDate('jurnal_umum.tanggal_jurnal', '<=', $to)->first()->debit;
+        $ppn_masukan = Detail_jurnal_umum::leftJoin('jurnal_umum', 'detail_jurnal_umum.id_jurnal_umum', '=', 'jurnal_umum.id')->select(DB::raw('cast(sum(detail_jurnal_umum.sub_total) as decimal(65,2)) as debit'))->where('detail_jurnal_umum.kode_akun_debit', '115.001')->whereBetween('jurnal_umum.tanggal_jurnal', [$from, $to])->first()->debit;
         // dd($ppn_keluaran);
-        // dd('hutang Pajak : '.$hutang_pajak. ', hutang pajak awal : '.$hutang_pajak_awal.' ppn keluaran : '.$ppn_keluaran.' ppn masukkan : '.$ppn_masukan);
+        dd('hutang Pajak : '.$hutang_pajak. ', hutang pajak awal : '.$hutang_pajak_awal.' ppn keluaran : '.$ppn_keluaran.' ppn masukkan : '.$ppn_masukan);
         $data['kas'] = number_format(round($kas_awal + $kas_debit - $kas_kredit), 2, ".", "");
         $data['bank'] = number_format(round($bank_awal + $bank_debit - $bank_kredit), 2, ".", "");
         $data['piutang_dagang'] = number_format(round($piutang_dagang_awal + $piutang_dagang_debit - $piutang_dagang_kredit), 2, ".", "");
         $data['uang_muka_pajak'] = number_format(round($uang_muka_pajak), 2, ".", "");
         $data['hutang_dagang'] = number_format(round($hutang_dagang), 2, ".", "");
+
         $hutang_pajak = $from < '2022-05-01' ? $ppn_keluaran+$hutang_pajak-$hutang_pajak_awal-$ppn_masukan : $ppn_keluaran-$hutang_pajak_awal-$ppn_masukan;
         $data['hutang_pajak'] = number_format(round($hutang_pajak), 2, ".", "");
         $data['modal'] = $modal_awal;
